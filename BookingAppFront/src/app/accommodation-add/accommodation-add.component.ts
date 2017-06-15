7d8a103b9371f684ca6d646567ae86b0572ec2b2
Marko Jelača <jelaca.marko@gmail.com>
2017-06-15 11:17:33 +0200

    Dodate metode za realizaciju uploada. Omoguceno cuvanje naziva uploadovane slike.

--- BookingAppFront/src/app/accommodation-add/accommodation-add.component.ts
+++ BookingAppFront/src/app/accommodation-add/accommodation-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms/src/forms";
+import { Accommodation } from "app/accommodation/accommodation";
 
 @Component({
   selector: 'app-accommodation-add',
@@ -7,18 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccommodationAddComponent implements OnInit {
 
+    uploadImageName: string;
+
   constructor() { }
 
+  onSubmitAccommodation(createdAccommodation: any, form: NgForm){
+      let accommodation: Accommodation;
+      
+
+  }
+
   imageUploaded(mess: any){
-      console.log("Image Uploaded");
+      console.log("Image Uploaded name:");
+      console.log(mess.file.name);
+      this.uploadImageName = mess.file.name;
   }
 
   imageRemoved(mess: any){
-      console.log("Image Removed");
+    //   console.log("Image Removed");
+    //   console.log(mess);
+    this.uploadImageName = "";
+    console.log("Image name:");
   }
 
   disableSendButton(mess: boolean){
-      console.log("Disable send button");
+    //   console.log("Disable send button");
+    //   console.log(mess);
   }
 
   ngOnInit() {
